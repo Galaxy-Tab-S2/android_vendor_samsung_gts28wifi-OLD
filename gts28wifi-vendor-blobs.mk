da01b2261d07c20d0024088ae45da0d5f0fc4707
CTXz <patrick_pedersen_02032001@yahoo.com>
2015-12-15 00:23:01 +0100

    Sensor : Add libsensorlistener.so
    Change-Id: Ia0d0149e1b7664809ec7c98ed8f94a37fab4a202

--- gts28wifi-vendor-blobs.mk
+++ gts28wifi-vendor-blobs.mk
@@ -47,6 +47,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gts28wifi/proprietary/vendor/lib/libRSDriverArm.so:system/vendor/lib/libRSDriverArm.so \
     vendor/samsung/gts28wifi/proprietary/lib/omx/libOMX.Exynos.AVC.Encoder.so:system/lib/omx/libOMX.Exynos.AVC.Encoder.so \
     vendor/samsung/gts28wifi/proprietary/lib/hw/sensors.exynos5.so:system/lib/hw/sensors.exynos5.so \
+    vendor/samsung/gts28wifi/proprietary/lib/libsensorlistener.so:system/lib/libsensorlistener.so \
     vendor/samsung/gts28wifi/proprietary/lib/liboemcrypto.so:system/lib/liboemcrypto.so \
     vendor/samsung/gts28wifi/proprietary/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
     vendor/samsung/gts28wifi/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
